db4def7bb44545221604b08a127fcf7d65275d01
Alex Hall <halla22@my.erau.edu>
2022-04-16 20:15:28 -0400

    fixed window function error

--- predict_path.py
+++ predict_path.py
@@ -1,31 +1,27 @@
 import os
+import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, to_timestamp
-from pyspark.sql.types import StructType, DoubleType, StringType, IntegerType
+from pyspark.sql.types import StructType, DoubleType, StringType, IntegerType, TimestampType
 from pyspark.sql.window import Window
 
-del os.environ["PYSPARK_SUBMIT_ARGS"]
-os.environ["PYSPARK_PYTHON"] = r"C:\Users\LethalCaffeine\AppData\Local\Programs\Python\Python38\python.exe"
+#del os.environ["PYSPARK_SUBMIT_ARGS"]
+os.environ["PYSPARK_PYTHON"] = r"C:\Python39\python.exe"
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages=org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 pyspark-shell'
 
 spark = SparkSession.builder \
-                    .master('local[*]') \
-                    .appName('CapstoneApp') \
+                    .master('local[2]') \
+                    .appName('predict') \
                     .getOrCreate()
 
 df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "127.0.0.1:9092").option("subscribe", "position_history_kinematic_aggs").load()
 
-features = df \
-    .selectExpr("cast(value as string)") \
-    .select(col("value").cast("string").alias("message"))
-
 features_schema = StructType() \
         .add("mmsi", IntegerType()) \
-        .add("window_start", StringType()) \
-        .add("window_end", StringType()) \
-        .add('moving_avg_sog', DoubleType()) \
-        .add('moving_avg_cog', DoubleType()) \
-        .add('moving_avg_rot', DoubleType()) \
+        .add("timestamp_utc", TimestampType()) \
+        .add('speed_over_ground', DoubleType()) \
+        .add('course_over_ground', DoubleType()) \
+        .add('rate_of_turn', DoubleType()) \
         .add('longitude', DoubleType()) \
         .add('latitude', DoubleType()) \
         .add('cartesian_x', DoubleType()) \
@@ -35,18 +31,77 @@ features = df \
     .selectExpr("cast(value as string)") \
     .select(from_json(col("value").cast("string"), features_schema).alias("message"))\
     .select("message.mmsi", 
-    "message.window_start", 
-    "message.window_end", 
-    "message.moving_avg_sog", 
-    "message.moving_avg_cog", 
-    "message.moving_avg_rot",
+    "message.timestamp_utc", 
+    "message.speed_over_ground", 
+    "message.course_over_ground", 
+    "message.rate_of_turn",
     "message.longitude",
     "message.latitude",
     "message.cartesian_x",
     "message.cartesian_y")
 
-features = features.withColumn("window_start_timestamp", to_timestamp(features.window_start, "yyyy-MM-dd'T'HH:mm:ss'Z'"))
-features = features.withColumn("window_end_timestamp", to_timestamp(features.window_end, "yyyy-MM-dd'T'HH:mm:ss'Z'"))
+columns = ["mmsi", "timestamp_utc", "speed_over_ground", "course_over_ground", "rate_of_turn",
+    "longitude", "latitude", "cartesian_x", "cartesian_y"]
+df_pandas = pd.DataFrame(data=[[-1, "2021-04-05 00:00:00", 5.6, 2.5, 2.5, 101.2, 45.3, 12485.0, 23958352.0, 1]], columns=columns)
+global vessel_positions
+vessel_positions = spark.createDataFrame(df_pandas)
+
+#features = features.withColumn("timestamp_utc", to_timestamp(features.timestamp_utc, "yyyy-MM-dd HH:mm:ss'Z'"))
+#features = features.withColumn("timestamp_utc", to_timestamp(features.timestamp_utc, "yyyy-MM-dd HH:mm:ss"))
+
+def foreach_batch_function(df, epoch_id):
+    df.persist()
+    global vessel_positions
+    print(vessel_positions)
+    df1 = vessel_positions.union(df)
+    df1.createOrReplaceTempView("current_positions")
+    df2 = SparkSession.getActiveSession().sql("""
+        with temp as (
+            select *, row_number() over (partition by mmsi order by timestamp_utc desc) as row_number 
+            from current_positions) 
+        select mmsi, timestamp_utc, speed_over_ground, course_over_ground, rate_of_turn, 
+            longitude, latitude, cartesian_x, cartesian_y 
+        from temp where row_number in (1,2,3)""".strip())
+    vessel_positions = df2
+
+features.writeStream.foreachBatch(foreach_batch_function).option("checkpointLocation", r"C:\Users\AlexHall97\Documents\kafka_checkpoints\test").trigger(processingTime="1 minute").start()  
+
+
+features_out = features.writeStream.outputMode("append") \
+    .format("csv") \
+    .option("path", r"C:\Users\AlexHall97\Documents\output") \
+    .option("checkpointLocation", r"C:\Users\AlexHall97\Documents\kafka_checkpoints\csv") \
+    .trigger(processingTime="30 seconds") \
+    .start()
+
+
+
+
+def foreach_batch_function(df, epoch_id):
+    # Transform and write batchDF
+    df.createOrReplaceTempView("batch")
+    current = spark.sql("select * from batch")
+    df_3 = spark.sql("""with temp as (select *, row_number() over (partition by mmsi order by timestamp_utc desc) as row_number from query q) 
+                    select * from temp where row_number in (1,2)""".strip())
+    vessel_positions = vessel_positions.alias('vp').join(
+        current.alias('c'), 
+        ['mmsi'], 
+        how='outer'
+    ).select("mmsi", "timestamp_utc", "speed_over_ground", "course_over_ground", "rate_of_turn",
+    "longitude", "latitude", "cartesian_x", "cartesian_y")
+    vessel_positions.createOrReplaceTempView("vessel_positions")
+    vessel_positions = spark.sql("""with temp as (select *, row_number() over (partition by mmsi order by timestamp_utc desc) as row_number from vessel_positions vp) 
+                    select * from temp where row_number in (1,2,3)""".strip())
+    vessel_positions.show()
+
+def foreach_function(row):
+    print(row)
+
+features.writeStream.foreach(foreach_function).option("checkpointLocation", r"C:\Users\AlexHall97\Documents\kafka_checkpoints\test").start()  
+
+
+# features = features.withColumn("window_start_timestamp", to_timestamp(features.window_start, "yyyy-MM-dd'T'HH:mm:ss'Z'"))
+# features = features.withColumn("window_end_timestamp", to_timestamp(features.window_end, "yyyy-MM-dd'T'HH:mm:ss'Z'"))
 
 # test = df.writeStream \
 #     .option("checkpointLocation", r"C:\Users\LethalCaffeine\Documents\kafka_checkpoints\features") \
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, asc, desc, from_json, to_timestamp, window, udf, avg, lag, last, first
+from pyspark.sql.functions import col, asc, desc, from_json, to_timestamp, window, udf, avg, lag, last, first, row_number
 from pyspark.sql.types import StructType, DoubleType, LongType, StringType, IntegerType, ArrayType
 from kafka import KafkaConsumer
 from haversine import haversine, Unit
@@ -8,13 +8,13 @@ from pyspark.sql.window import Window
 import re
 from _util import geodetic_to_geocentric
 
-del os.environ["PYSPARK_SUBMIT_ARGS"]
-os.environ["PYSPARK_PYTHON"] = r"C:\Users\LethalCaffeine\AppData\Local\Programs\Python\Python38\python.exe"
+#del os.environ["PYSPARK_SUBMIT_ARGS"]
+os.environ["PYSPARK_PYTHON"] = r"C:\Python39\python.exe"
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages=org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 pyspark-shell'
 
 spark = SparkSession.builder \
-        .master('local[*]') \
-        .appName('CapstoneApp') \
+        .master('local[1]') \
+        .appName('preprocess') \
         .getOrCreate()
 
 df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "127.0.0.1:9092").option("subscribe", "position_history").load()
@@ -64,65 +64,26 @@ df_query = df_query.drop('position', 'geodesic_coordinates', 'cartesian_coordina
 # There is a Z char to represent zulu time i.e. UTC format. Enclose the Z char with single quotes so the to_timestamp function properly parses the datetime input
 df_query = df_query.withColumn("timestamp_utc", to_timestamp(df_query.timestamp_utc, "yyyy-MM-dd HH:mm:ss'Z'"))
 
-df_windowavg = df_query.withWatermark("timestamp_utc", "5 minutes").groupBy(
-        window(timeColumn=df_query.timestamp_utc, windowDuration="1 minute", slideDuration="15 seconds"), df_query.mmsi).agg(
-                avg("speed_over_ground").alias("moving_avg_sog"), 
-                avg("course_over_ground").alias("moving_avg_cog"), 
-                avg("rate_of_turn").alias("moving_avg_rot"),
-                last("longitude").alias("longitude"),
-                last("latitude").alias("latitude"),
-                last("cartesian_x").alias("cartesian_x"),
-                last("cartesian_y").alias("cartesian_y")
-        )
-
-df_windowavg_timewindow = df_windowavg.select("mmsi",
-        col("window.start").alias("window_start"),
-        col("window.end").alias("window_end"),
-        col("moving_avg_sog"),
-        col("moving_avg_cog"),
-        col("moving_avg_rot"),
-        col("longitude"),
-        col("latitude"),
-        col("cartesian_x"),
-        col("cartesian_y")
-        ).orderBy(asc("mmsi"), asc("window_start"))
-
-# working off of onedrive so I specify the checkpointLocation to a folder on my computer for easy removal
-send_aggregations_to_kafka = df_windowavg_timewindow \
+send_aggregations_to_kafka = df_query \
         .selectExpr("to_json(struct(*)) AS value") \
-        .writeStream.outputMode("complete") \
+        .writeStream.outputMode("update") \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
         .option("topic", "position_history_kinematic_aggs") \
-        .option("checkpointLocation", r"C:\Users\LethalCaffeine\Documents\kafka_checkpoints\preprocess") \
-        .start()
-
-df_windowavg_timewindow.createOrReplaceTempView("updates")
-
-df_windowavg._jdf.sparkSession().sql("""
-        MERGE INTO lastestData t
-        USING updates s
-        ON s.mmsi = t.mmsi
-        WHEN MATCHED THEN UPDATE SET *
-        WHEN NOT MATCHED THEN INSERT *
-""")
-
-
-# TODO Detect and remove erroneous speed_over_ground
-# This will need to be a window passed operation since we need to replace the value with a previous valid SOG
-
-# TODO Interpolate the position values for every minute
-# Window passed operation
-
-# valid outputMode options are ["append", "complete", "update"]
-# query = df_windowavg_timewindow.writeStream.outputMode("complete").format("console").trigger(processingTime="10 seconds").start()
-send_aggregations_to_kafka = df_windowavg_timewindow \
-        .selectExpr("to_json(struct(*)) AS value") \
-        .writeStream.outputMode("complete") \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
-        .option("topic", "position_history_kinematic_aggs") \
-        .option("checkpointLocation", "checkpoint/send_to_kafka") \
+        .option("checkpointLocation", r"C:\Users\AlexHall97\Documents\kafka_checkpoints\preprocess") \
         .start()
 
 send_aggregations_to_kafka.awaitTermination()
+
+# df_windowavg = df_query.withWatermark("timestamp_utc", "5 minutes").groupBy(
+#         window(timeColumn=df_query.timestamp_utc, windowDuration="2 minutes", slideDuration="1 minute"), df_query.mmsi).agg(
+#                 avg("speed_over_ground").alias("moving_avg_sog"), 
+#                 avg("course_over_ground").alias("moving_avg_cog"), 
+#                 avg("rate_of_turn").alias("moving_avg_rot"),
+#                 last("longitude").alias("longitude"),
+#                 last("latitude").alias("latitude"),
+#                 last("cartesian_x").alias("cartesian_x"),
+#                 last("cartesian_y").alias("cartesian_y"),
+                
+#         )
+
